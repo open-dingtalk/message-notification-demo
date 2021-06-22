afe7a5ecf05fb1cad760e8cd289ea11e2012f40e
wanzhiqiang <15638940333@163.com>
2021-06-22 09:39:01 +0800

    initial project

--- src/main/java/com/aliyun/dingtalk/controller/MessageNotificationController.java
+++ src/main/java/com/aliyun/dingtalk/controller/MessageNotificationController.java
@@ -5,7 +5,6 @@ import com.aliyun.dingtalk.model.RpcServiceResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
