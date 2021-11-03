960f49b7276c134e7a4d66493574460fc2434f4e
wb-zr624549 <wb-zr624549@alibaba-inc.com>
2021-11-03 15:16:11 +0800

    feat:test

--- fronted/src/components/Group.js
+++ fronted/src/components/Group.js
@@ -10,17 +10,17 @@ function getBase64(img, callback) {
   reader.readAsDataURL(img)
 }
 
-function beforeUpload(file) {
-  const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png"
-  if (!isJpgOrPng) {
-    message.error("You can only upload JPG/PNG file!")
-  }
-  const isLt2M = file.size / 1024 / 1024 < 2
-  if (!isLt2M) {
-    message.error("Image must smaller than 2MB!")
-  }
-  return isJpgOrPng && isLt2M
-}
+// function beforeUpload(file) {
+//   const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png"
+//   if (!isJpgOrPng) {
+//     message.error("You can only upload JPG/PNG file!")
+//   }
+//   const isLt2M = file.size / 1024 / 1024 < 2
+//   if (!isLt2M) {
+//     message.error("Image must smaller than 2MB!")
+//   }
+//   return isJpgOrPng && isLt2M
+// }
 
 const uploadMedia = (data) => {
   // 上传图片
@@ -122,9 +122,10 @@ const Group = (props) => {
             listType="picture-card"
             className="avatar-uploader"
             showUploadList={false}
-            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-            // /upload
-            beforeUpload={beforeUpload}
+            action="/upload"
+            // https://www.mocky.io/v2/5cc8019d300000980a055e76
+            // beforeUpload={beforeUpload}
+            headers={{ "Content-Type": "application/json" }}
             onChange={handleChange}
           >
             {imageUrl ? (
