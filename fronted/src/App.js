d37b8789b3ba6536136238fdd356db825bdeab90
wb-zr624549 <wb-zr624549@alibaba-inc.com>
2021-11-03 15:10:38 +0800

    feat:优化前端页面

--- fronted/src/App.js
+++ fronted/src/App.js
@@ -1,169 +1,214 @@
-import './App.css';
-import axios from 'axios';
-import * as dd from 'dingtalk-jsapi';
+import react, { useEffect, useState } from "react"
+import "./App.css"
+import axios from "axios"
+import * as dd from "dingtalk-jsapi"
+import { Button } from "antd"
+import Group from "./components/Group"
+import "antd/dist/antd.min.css"
 
 //内网穿透工具介绍:
 // https://developers.dingtalk.com/document/resourcedownload/http-intranet-penetration?pnamespace=app
 // 替换成后端服务域名
-export const domain = "";
+export const domain = ""
 
 function App() {
-
-    const uploadMedia = () => {
-        // 获取存储的用户部门和ID
-        const data = {};
-        // 发起会议
-        axios({
-            url: domain + '/upload',
-            method: 'post',
-            data: data,
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-            .then(function (response) {
-                // alert(JSON.stringify(response));
-                console.log(response);
-                sessionStorage.setItem("mediaId", response.data.data);
-
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    };
-
-    const sendGroupMessage = () => {
-        // 获取存储的用户部门和ID
-        const userId = sessionStorage.getItem('userId');
-        const userIdList = sessionStorage.getItem('userIdList');
-        const deptId = sessionStorage.getItem('deptId');
-        // demo直接构建了要请求的数据，实际开发需要从页面获取
-        let url = 'https://www.dingtalk.com';
-
-        const data = {
-            "owner": userId,
-            "name" : "群消息",
-            "userIdList" : [userIdList],
-            "dingTalkMessage" : {
-                "msgType" : "link",
-                "link" :{
-                    "messageUrl" : "dingtalk://dingtalkclient/page/link?url=" + encodeURIComponent(url) + "&pc_slide=true",
-                    "picUrl" : sessionStorage.getItem('mediaId'),
-                    "title" : "测试",
-                    "text" : "测试",
-                }
-            }
-        };
-        // 发起会议
-        axios({
-            url: domain + '/message/group',
-            method: 'post',
-            data: data,
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-            .then(function (response) {
-                // alert(JSON.stringify(response));
-                console.log(response);
-                sessionStorage.setItem("messageId", response.data.data);
-
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    };
-
-    const readUserList = () => {
-
-        // 获取已读人员列表
-        axios.get(domain + '/message/group/' + sessionStorage.getItem("messageId"))
-            .then(function (response) {
-                alert(JSON.stringify(response.data.data))
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    };
-
-    return (
-        <div className="App">
-            {/*<header className="App-header">*/}
-            {/*<button onClick={createMeeting}>领用并提交审批</button>*/}
-            {/*</header>*/}
-            <header className="App-header">
-                <button onClick={uploadMedia}>上传媒体文件</button>
-                <button onClick={sendGroupMessage}>发送群消息</button>
-                <button onClick={readUserList}>查看已读人员列表</button>
-            </header>
-            {/*<div className="container">*/}
-            {/*    <List/>*/}
-            {/*</div>*/}
-        </div>
-    );
-};
-
-
-dd.ready(function () {
-    // let corpId;
-    // axios.get(domain + "/config")
-    //     .then(response => {
-    //         corpId = response.data.corpId;
-    //     })
-    //     .catch(error => {
-    //         alert(JSON.stringify(error))
-    //         // console.log(error.message)
-    //     })
-    let corpId;
-    fetch(domain + '/config')
-        .then(res => res.json())
+  const [showType, setShowType] = useState(0)
+  const [userIdList, setUserIdList] = useState([])
+  useEffect(() => {
+    return
+    dd.ready(function () {
+      // let corpId;
+      // axios.get(domain + "/config")
+      //     .then(response => {
+      //         corpId = response.data.corpId;
+      //     })
+      //     .catch(error => {
+      //         alert(JSON.stringify(error))
+      //         // console.log(error.message)
+      //     })
+      let corpId
+      fetch(domain + "/config")
+        .then((res) => res.json())
         .then((result) => {
-            // alert(JSON.stringify(result));
-            corpId = result.data.corpId;
-            // dd.ready参数为回调函数，在环境准备就绪时触发，jsapi的调用需要保证在该回调函数触发后调用，否则无效。
-            dd.runtime.permission.requestAuthCode({
+          // alert(JSON.stringify(result));
+          corpId = result.data.corpId
+          // dd.ready参数为回调函数，在环境准备就绪时触发，jsapi的调用需要保证在该回调函数触发后调用，否则无效。
+          dd.runtime.permission.requestAuthCode({
+            corpId: corpId, //三方企业ID
+            onSuccess: function (result) {
+              // alert(JSON.stringify(result));
+              axios
+                .get(domain + "/login?authCode=" + result.code)
+                .then((response) => {
+                  // alert(JSON.stringify(response));
+                  // alert(JSON.stringify(response.data));
+                  // alert(JSON.stringify(response.data.data.userid));
+                  // alert(JSON.stringify(response.data.data.deptIdList[0]));
+                  // 登录成功后储存用户部门和ID
+                  sessionStorage.setItem("userId", response.data.data.userid)
+                  sessionStorage.setItem("unionId", response.data.data.unionid)
+                  sessionStorage.setItem(
+                    "deptId",
+                    response.data.data.deptIdList[0]
+                  )
+                  const qs = require("qs")
+                  // 获取部门部门用户详情
+                  axios
+                    .get(domain + "/users", {
+                      params: {
+                        deptIds: response.data.data.deptIdList,
+                      },
+                      paramsSerializer: function (params) {
+                        return qs.stringify(params, { arrayFormat: "repeat" })
+                      },
+                    })
+                    .then((response) => {
+                      console.log(response)
+                      // 此处为创建人的userId，可以使用response里面返回的数据
+                      //   sessionStorage.setItem(
+                      //     "userIdList",
+                      //     sessionStorage.getItem("userId")
+                      //   )
+                      setUserIdList(response.data.data)
+                    })
+                    .catch((error) => {
+                      alert(JSON.stringify(error))
+                    })
+                })
+                .catch((error) => {
+                  alert(JSON.stringify(error))
+                  // console.log(error.message)
+                })
+            },
+            onFail: function (err) {
+              alert(JSON.stringify(err))
+            },
+          })
+        })
+    })
+  }, [])
 
-                corpId: corpId, //三方企业ID
-                onSuccess: function (result) {
-                    // alert(JSON.stringify(result));
-                    axios.get(domain + "/login?authCode=" + result.code)
-                        .then(response => {
-                            // alert(JSON.stringify(response));
-                            // alert(JSON.stringify(response.data));
-                            // alert(JSON.stringify(response.data.data.userid));
-                            // alert(JSON.stringify(response.data.data.deptIdList[0]));
-                            // 登录成功后储存用户部门和ID
-                            sessionStorage.setItem("userId", response.data.data.userid);
-                            sessionStorage.setItem("unionId", response.data.data.unionid);
-                            sessionStorage.setItem("deptId", response.data.data.deptIdList[0]);
-                            const qs = require('qs');
-                            // 获取部门部门用户详情
-                            axios.get(domain + "/users", {
-                                params: {
-                                    deptIds: response.data.data.deptIdList,
-                                },
-                                paramsSerializer: function (params) {
-                                    return qs.stringify(params, {arrayFormat: 'repeat'});
-                                }
-                            }).then(response => {
-                                console.log(response);
-                                // 此处为创建人的userId，可以使用response里面返回的数据
-                                sessionStorage.setItem("userIdList", sessionStorage.getItem("userId"));
-                            }).catch(error => {
-                                alert(JSON.stringify(error));
-                            })
-                        })
-                        .catch(error => {
-                            alert(JSON.stringify(error));
-                            // console.log(error.message)
-                        })
+  //   const uploadMedia = () => {
+  //     // 上传图片
+  //     const data = {}
+  //     // 发起会议
+  //     axios({
+  //       url: domain + "/upload",
+  //       method: "post",
+  //       data: data,
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //     })
+  //       .then(function (response) {
+  //         // alert(JSON.stringify(response));
+  //         console.log(response)
+  //         sessionStorage.setItem("mediaId", response.data.data)
+  //       })
+  //       .catch(function (error) {
+  //         console.log(error)
+  //       })
+  //   }
 
-                },
-                onFail: function (err) {
-                    alert(JSON.stringify(err))
-                }
-            });
-        });
-});
+  const sendGroupMessage = (data) => {
+    // 发送群消息
+    // const userId = sessionStorage.getItem("userId")
+    // demo直接构建了要请求的数据，实际开发需要从页面获取
+    // let url = "https://www.dingtalk.com"
+    // {
+    //     owner: userId,
+    //     name: "群消息",
+    //     userIdList,
+    //     dingTalkMessage: {
+    //       msgType: "link",
+    //       link: {
+    //         messageUrl,
+    //         picUrl,
+    //         title,
+    //         text,
+    //       },
+    //     },
+    //   }
+    // const data = {
+    //   owner: userId,
+    //   name: "群消息",
+    //   userIdList: [userIdList],
+    //   dingTalkMessage: {
+    //     msgType: "link",
+    //     link: {
+    //       messageUrl:
+    //         "dingtalk://dingtalkclient/page/link?url=" +
+    //         encodeURIComponent(url) +
+    //         "&pc_slide=true",
+    //       picUrl: sessionStorage.getItem("mediaId"),
+    //       title: "测试",
+    //       text: "测试",
+    //     },
+    //   },
+    // }
+    // 发送群消息
+    axios({
+      url: domain + "/message/group",
+      method: "post",
+      data: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(function (response) {
+        // alert(JSON.stringify(response));
+        setShowType(false)
+        console.log(response)
+        sessionStorage.setItem("messageId", response.data.data)
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  }
 
-export default App;
+  const readUserList = async () => {
+    // 获取已读人员列表
+    axios
+      .get(domain + "/message/group/" + sessionStorage.getItem("messageId"))
+      .then(function (response) {
+        setUserIdList(response.data.data)
+        alert(JSON.stringify(response.data.data))
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+    await setShowType(2)
+  }
+
+  return (
+    <div className="App">
+      {/*<header className="App-header">*/}
+      {/*<button onClick={createMeeting}>领用并提交审批</button>*/}
+      {/*</header>*/}
+      {showType === 1 && (
+        <Group
+          onClick={(params) => sendGroupMessage(params)}
+          userIdList={userIdList}
+        />
+      )}
+      <header className="App-header">
+        {/* <Button onClick={uploadMedia}>上传媒体文件</Button> */}
+        {!showType && (
+          <Button type="primary" onClick={() => setShowType(1)}>
+            发送群消息
+          </Button>
+        )}
+        {!showType && (
+          <Button type="primary" onClick={readUserList}>
+            查看已读人员列表
+          </Button>
+        )}
+      </header>
+      {/*<div className="container">*/}
+      {/*    <List/>*/}
+      {/*</div>*/}
+    </div>
+  )
+}
+
+export default App
---
+++ fronted/src/components/Group.js
@@ -0,0 +1,161 @@
+import react, { useEffect, useState } from "react"
+import { Form, Input, Button, Checkbox, Upload, message } from "antd"
+import { LoadingOutlined, PlusOutlined } from "@ant-design/icons"
+import { ImageUploader } from "antd-mobile"
+import axios from "axios"
+
+function getBase64(img, callback) {
+  const reader = new FileReader()
+  reader.addEventListener("load", () => callback(reader.result))
+  reader.readAsDataURL(img)
+}
+
+function beforeUpload(file) {
+  const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png"
+  if (!isJpgOrPng) {
+    message.error("You can only upload JPG/PNG file!")
+  }
+  const isLt2M = file.size / 1024 / 1024 < 2
+  if (!isLt2M) {
+    message.error("Image must smaller than 2MB!")
+  }
+  return isJpgOrPng && isLt2M
+}
+
+const uploadMedia = (data) => {
+  // 上传图片
+  // 发起会议
+  const domain = ""
+  axios({
+    url: domain + "/upload",
+    method: "post",
+    data: data,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then(function (response) {
+      // alert(JSON.stringify(response));
+      console.log(response)
+      sessionStorage.setItem("mediaId", response.data.data)
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+}
+
+const Group = (props) => {
+  const [form] = Form.useForm()
+  const [loading, setLoading] = useState(false)
+  const [imageUrl, setImageUrl] = useState("")
+
+  const uploadButton = (
+    <div>
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </div>
+  )
+  const handleChange = (info) => {
+    if (info.file.status === "uploading") {
+      setLoading(true)
+      return
+    }
+    if (info.file.status === "done") {
+      getBase64(info.file.originFileObj, (file) => {
+        setImageUrl(file)
+        setLoading(false)
+      })
+    }
+  }
+
+  useEffect(() => {
+    // form.setFieldsValue({
+    //   title: "任务待办",
+    //   url: "/toWork",
+    //   createTime: "2021-07-05 16:00:00",
+    //   formTitle: "标题",
+    //   formContent: "内容",
+    // })
+  }, [])
+
+  const onSubmit = (data) => {
+    const userId = sessionStorage.getItem("userId")
+    console.log(data, "-----")
+    const { userIdList, messageUrl, title, text, file } = data
+    const picUrl = file?.file?.response?.url
+    const params = {
+      owner: userId,
+      name: "群消息",
+      userIdList,
+      dingTalkMessage: {
+        msgType: "link",
+        link: {
+          messageUrl,
+          picUrl,
+          title,
+          text,
+        },
+      },
+    }
+
+    props.onClick(params)
+  }
+
+  return (
+    <div>
+      <h4 className="title">创建群消息</h4>
+      <Form form={form} onFinish={onSubmit}>
+        <Form.Item label="群名称" name="name">
+          <Input placeholder="请输入群名称" />
+        </Form.Item>
+        <Form.Item label="消息标题" name="title">
+          <Input placeholder="请输入消息标题" />
+        </Form.Item>
+        <Form.Item label="消息内容" name="text">
+          <Input placeholder="请输入消息内容" />
+        </Form.Item>
+        <Form.Item label="消息链接" name="messageUrl">
+          <Input placeholder="请输入消息链接" />
+        </Form.Item>
+        <Form.Item label="消息图片" name="file">
+          <Upload
+            listType="picture-card"
+            className="avatar-uploader"
+            showUploadList={false}
+            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+            // /upload
+            beforeUpload={beforeUpload}
+            onChange={handleChange}
+          >
+            {imageUrl ? (
+              <img src={imageUrl} alt="avatar" style={{ width: "100%" }} />
+            ) : (
+              uploadButton
+            )}
+          </Upload>
+        </Form.Item>
+        <Form.Item label="选择发送人" name="userIdList">
+          <Checkbox.Group>
+            {/* onChange={onChange} */}
+            {/*  [
+              { name: "11111", userid: "22222" },
+              { name: "333", userid: "444" },
+            ]*/}
+            {props.userIdList.map((item, i) => (
+              <div key={"userid" + i}>
+                <Checkbox value={item.userId} name={item.name}>
+                  {item.name}
+                </Checkbox>
+              </div>
+            ))}
+          </Checkbox.Group>
+        </Form.Item>
+        <Button htmlType="submit" type="primary">
+          提交
+        </Button>
+      </Form>
+    </div>
+  )
+}
+
+export default Group
