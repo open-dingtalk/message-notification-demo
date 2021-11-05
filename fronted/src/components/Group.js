c52057416f7e8897588f02c63fc6afdd2c83422a
wb-zr624549 <wb-zr624549@alibaba-inc.com>
2021-11-05 15:31:35 +0800

    feat:test

--- fronted/src/components/Group.js
+++ fronted/src/components/Group.js
@@ -124,8 +124,8 @@ const Group = (props) => {
             listType="picture-card"
             className="avatar-uploader"
             showUploadList={false}
-            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-            // /upload
+            action="/upload"
+            // https://www.mocky.io/v2/5cc8019d300000980a055e76
             // beforeUpload={beforeUpload}
             onChange={handleChange}
           >
